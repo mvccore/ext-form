c9e1004931ec720ef913c8ec80498cd291981103
Tom Flídr <tomflidr@gmail.com>
2022-10-31 23:38:55 +0100

    Hidden field with disabled, readonly, required and min/max length.

--- src/MvcCore/Ext/Forms/Fields/Hidden.php
+++ src/MvcCore/Ext/Forms/Fields/Hidden.php
@@ -24,6 +24,10 @@ namespace MvcCore\Ext\Forms\Fields;
 class	Hidden 
 extends	\MvcCore\Ext\Forms\Field {
 
+	use \MvcCore\Ext\Forms\Field\Props\Disabled;
+	use \MvcCore\Ext\Forms\Field\Props\ReadOnlyAttr;
+	use \MvcCore\Ext\Forms\Field\Props\Required;
+	use \MvcCore\Ext\Forms\Field\Props\MinMaxLength;
 	use \MvcCore\Ext\Forms\Field\Props\AutoComplete;
 
 	/**
@@ -86,6 +90,44 @@ extends	\MvcCore\Ext\Forms\Field {
 	 * Every typed field has it's own predefined validators, but you can define any
 	 * validator you want and replace them.
 	 * 
+	 * @param  bool                   $disabled
+	 * Form field attribute `disabled`, determination if field value will be 
+	 * possible to change by user and if user will be graphically informed about it 
+	 * by default browser behaviour or not. Default value is `FALSE`. 
+	 * This flag is also used for sure for submit checking. But if any field is 
+	 * marked as disabled, browsers always don't send any value under this field name
+	 * in submit. If field is configured as disabled, no value sent under field name 
+	 * from user will be accepted in submit process and value for this field will 
+	 * be used by server side form initialization. 
+	 * Disabled attribute has more power than required. If disabled is true and
+	 * required is true and if there is no or invalid submitted value, there is no 
+	 * required error and it's used value from server side assigned by 
+	 * `$form->SetValues();` or from session.
+	 * @param  bool                   $readOnly
+	 * Form field attribute `readonly`, determination if field value will be 
+	 * possible to read only or if value will be possible to change by user. 
+	 * Default value is `FALSE`. This flag is also used for submit checking. 
+	 * If any field is marked as read only, browsers always send value in submit.
+	 * If field is configured as read only, no value sent under field name 
+	 * from user will be accepted in submit process and value for this field 
+	 * will be used by server side form initialization. 
+	 * Readonly attribute has more power than required. If readonly is true and
+	 * required is true and if there is invalid submitted value, there is no required 
+	 * error and it's used value from server side assigned by 
+	 * `$form->SetValues();` or from session.
+	 * @param  bool                   $required
+	 * Form field attribute `required`, determination
+	 * if control will be required to complete any value by user.
+	 * This flag is also used for submit checking. Default value is `NULL`
+	 * to not require any field value. If form has configured it's property
+	 * `$form->GetDefaultRequired()` to `TRUE` and this value is `NULL`, field
+	 * will be automatically required by default form configuration.
+	 * 
+	 * @param  int                    $minLength
+	 * Minimum characters length. Default value is `NULL`.
+	 * @param  int                    $maxLength
+	 * Maximum characters length. Default value is `NULL`.
+	 * 
 	 * @param  string                 $autoComplete 
 	 * Attribute indicates if the input can be automatically completed 
 	 * by the browser, usually by remembering previous values the user 
@@ -105,6 +147,13 @@ extends	\MvcCore\Ext\Forms\Field {
 		$translate = NULL, 
 		array $controlAttrs = [], 
 		array $validators = [],
+
+		$disabled = NULL,
+		$readOnly = NULL,
+		$required = NULL,
+
+		$minLength = NULL,
+		$maxLength = NULL,
 
 		$autoComplete = NULL
 	) {
