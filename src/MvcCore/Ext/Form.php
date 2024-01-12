5fc9dd9bafb1ee7f1e3b0c308c3b2e55b40dd867
Tom Flídr <tomflidr@gmail.com>
2024-01-12 16:39:22 +0100

    release v5.2.16

--- src/MvcCore/Ext/Form.php
+++ src/MvcCore/Ext/Form.php
@@ -28,7 +28,7 @@ implements	\MvcCore\Ext\IForm {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.15';
+	const VERSION = '5.2.16';
 
 	use \MvcCore\Ext\Form\InternalProps;
 	use \MvcCore\Ext\Form\ConfigProps;
