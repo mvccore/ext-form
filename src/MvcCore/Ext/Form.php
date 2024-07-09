b0a370252ae9b40ea55e3626c14ca5495ca1cbc6
Tom Flídr <tomflidr@gmail.com>
2024-07-09 14:04:37 +0200

    release v5.2.24

--- src/MvcCore/Ext/Form.php
+++ src/MvcCore/Ext/Form.php
@@ -28,7 +28,7 @@ implements	\MvcCore\Ext\IForm {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.23';
+	const VERSION = '5.2.24';
 
 	/**
 	 * Initial value after form has been instantiated.
